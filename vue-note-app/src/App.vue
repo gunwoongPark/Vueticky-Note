346efdea6b75a4d0869c4148bb9e47b846c6e19d
20160450 <olympiodoros@naver.com>
2020-10-02 01:06:02 +0900

    change indenting

--- vue-note-app/src/App.vue
+++ vue-note-app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
+<div id="app">
     <app-header @openEditor="editorOpen = !editorOpen"></app-header>
-    <app-note-editor v-if="editorOpen" @noteAdded="newNote" @noteDeleted="deleteNote"></app-note-editor>        
+    <app-note-editor v-if="editorOpen" @noteAdded="newNote" @noteDeleted="deleteNote"></app-note-editor>
     <div class="noteContainer">
         <div v-for="(note, index) in notes" :key="`note-${index}`" class="note" :style="{'background-color': note.theme}">
             <div>
-                <span class="delete" @click.prevent="deleteNote(index)"><i class="fas fa-times"></i></span> 
-                <span >{{ note.title}}</span>
+                <span class="delete" @click.prevent="deleteNote(index)"><i class="fas fa-times"></i></span>
+                <span>{{ note.title}}</span>
                 <p class="note-text">{{ note.text }}</p>
             </div>
         </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -19,54 +19,57 @@ import NoteEditor from './components/NoteEditor.vue';
 import Header from './components/Header.vue';
 
 export default {
-  name: 'App',
-  data: function() {
-    return {
-      editorOpen: false,
-      notes: [
-        {
-          title: 'Code',
-          text: '1131111222',
-          theme: '#FF8A80',
+    name: 'App',
+    data: function () {
+        return {
+            editorOpen: false,
+            notes: [{
+                    title: 'Code',
+                    text: '1131111222',
+                    theme: '#FF8A80',
+                },
+                {
+                    title: 'event',
+                    text: 'event',
+                    theme: '#DDA0DD',
+                },
+            ],
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        newNote(title, text, theme) {
+            this.notes.push({
+                title: title,
+                text: text,
+                theme: theme
+            });
         },
-        {
-          title: 'event',
-          text: 'event',
-          theme: '#DDA0DD',
+        deleteNote(index) {
+            this.notes.splice(index, 1)
         },
-      ],      
+    },
+    mounted() {
+        if (localStorage.getItem('notes')) this.notes = JSON.parse(localStorage.getItem('notes'));
+    },
+    watch: {
+        notes: {
+            handler() {
+                var newNotes = this.notes;
+                localStorage.setItem('notes', JSON.stringify(newNotes));
+            },
+            deep: true,
+        },
+    },
+    components: {
+        appNoteEditor: NoteEditor,
+        appHeader: Header
     }
-  },
-	computed: {
-		
-	},
-  methods: {   
-    newNote(title, text, theme) {
-      this.notes.push({title: title, text: text, theme: theme});
-    },
-    deleteNote(index) {
-      this.notes.splice(index, 1)
-    },
-  },
-  mounted() {
-    if (localStorage.getItem('notes')) this.notes = JSON.parse(localStorage.getItem('notes'));
-  },
-  watch: {
-    notes: {
-      handler() {
-        var newNotes = this.notes;
-        localStorage.setItem('notes', JSON.stringify(newNotes));
-      },
-      deep: true,
-    },
-  },
-  components: {
-    appNoteEditor: NoteEditor,
-    appHeader: Header
-  }
 }
 </script>
 
 <style lang="scss">
-  @import '/styles/global.scss';
+@import '/styles/global.scss';
 </style>
--- vue-note-app/src/components/NoteEditor.vue
+++ vue-note-app/src/components/NoteEditor.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="note-grid">
-        <div class="note-editor">
-            <input class="title-input" type="text" v-model="title" placeholder="Title">
-            <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>            
-            <div class="note-editor-bottom">
-                <button @click=createNew class="fas fas-check-circle"><i class="fas fa-check-circle"></i></button>
-            </div>
+<div class="note-grid">
+    <div class="note-editor">
+        <input class="title-input" type="text" v-model="title" placeholder="Title">
+        <textarea rows="10" v-model="text" placeholder="Take a note..."></textarea>
+        <div class="note-editor-bottom">
+            <button @click=createNew class="fas fas-check-circle"><i class="fas fa-check-circle"></i></button>
         </div>
     </div>
+</div>
 </template>
 
 <script>
-
 export default {
-    data: function() {
+    data: function () {
         return {
-            title: '',            
-            theme: '',            
+            title: '',
+            theme: '',
             text: ''
         }
-    },   
-    methods: {        
+    },
+    methods: {
         createNew() {
             this.$emit('noteAdded', this.title, this.text, this.theme);
             this.title = '';
             this.text = '',
-            this.theme = '';
+                this.theme = '';
         },
         deleteNote(index) {
             this.$emit('noteDeleted', index);
