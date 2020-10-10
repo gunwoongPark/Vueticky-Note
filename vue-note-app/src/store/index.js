240ba2174f2da21ad8d8ac5bceb91e375f92490f
20160450 <olympiodoros@naver.com>
2020-10-11 05:23:34 +0900

    setting vuex 2

--- vue-note-app/src/store/index.js
+++ vue-note-app/src/store/index.js
@@ -4,5 +4,26 @@ import vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    strict: process.env.NODE_ENV !== 'production'
+
+    // data
+    state:()=>({
+        
+    }),
+
+    // computed
+    getters:{
+
+    },
+
+    // methods
+    // 실제 값을 변경할 때(비동기X)
+    mutations:{
+
+    },
+
+    // methods
+    // 일반 로직(비동기O)
+    actions:{
+
+    }
 })
