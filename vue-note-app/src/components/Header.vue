21468596e9f942b74d65edb72a12b0fe01bbc3e5
20161313 <dbstjd427@gmail.com>
2020-10-18 00:27:58 +0900

    add Desgin on master

--- vue-note-app/src/components/Header.vue
+++ vue-note-app/src/components/Header.vue
@@ -1,53 +1,54 @@
 <template>
-<div class="header">
-    <img src="../assets/50Vemo.png" />
-    <p>Vemo</p>
-    <SearchBar @submitText="submitText" />
+  <div class="header">
+    <img src="../assets/50VemoBlue.png" />
     <v-spacer></v-spacer>
-    <p>{{ this.date }}</p>
-</div>
+    <p>Now Date is {{ this.date }}</p>
+    <v-spacer></v-spacer>
+    <SearchBar @submitText="submitText" />
+  </div>
 </template>
 
 <script>
 import SearchBar from './SearchBar'
 export default {
-    props: {
-        date: {
-            type: String,
-            required: true,
-        },
+  props: {
+    date: {
+      type: String,
+      required: true,
     },
+  },
 
-    methods: {
-        submitText(text) {
-            this.$emit("searchNote", text);
-        }
-    },
-
-    components: {
-        SearchBar
+  methods: {
+    submitText (text) {
+      this.$emit("searchNote", text);
     }
+  },
+
+  components: {
+    SearchBar
+  }
 };
 </script>
 
 <style scoped>
 .header {
-    display: flex;
-    align-items: center;
-    background: #000;
-    color: #fff;
-    position: fixed;
-    left: 0;
-    right: 0;
+  display: flex;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.741);
+  color: #2196f3;
+  position: fixed;
+  left: 0;
+  right: 0;
 }
 
 img {
-    margin-left: 20px;
+  margin-left: 20px;
 }
 
 p {
-    font-size: 25px;
-    margin: 25px;
-    margin-left: 10px;
+  font-size: 25px;
+  margin: 25px;
+  margin-left: 11%;
+  font-family: "Sansita Swashed", cursive;
 }
 </style>
