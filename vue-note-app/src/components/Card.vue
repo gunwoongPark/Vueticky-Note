c1e7ce557100abbb89ed9efd202f80e7bc171fb6
20160450 <olympiodoros@naver.com>
2020-10-20 23:42:29 +0900

    confirm delete

--- vue-note-app/src/components/Card.vue
+++ vue-note-app/src/components/Card.vue
@@ -1,127 +1,120 @@
 <template>
-  <div>
+<div>
     <v-dialog v-model="dialog" width="550">
-      <template v-slot:activator="{ on, attrs }">
-        <v-card
-          @mouseenter="mouseEnter"
-          @mouseleave="mouseLeave"
-          class="note"
-          :style="{ 'background-color': note.theme }"
-        >
-          <div class="Container">
-            <v-icon v-if="note.important" class="starIcon">mdi-pin</v-icon>
-            <v-spacer></v-spacer>
-            <div class="iconContainer">
-              <v-icon class="deleteIcon" @click.prevent="deleteNote"
-                >mdi-close-circle</v-icon
-              >
-            </div>
-          </div>
-          <div v-bind="attrs" v-on="on" @click.prevent="initData">
-            <v-card-title class="cardTitle noteTitle">
-              <p>{{ note.title }}</p>
-            </v-card-title>
-            <v-card-text class="noteText">
-              <p>{{ note.text }}</p>
-            </v-card-text>
-          </div>
-        </v-card>
-      </template>
-      <ModifyEditor :note="note" @noteModified="modifyNote" />
+        <template v-slot:activator="{ on, attrs }">
+            <v-card @mouseenter="mouseEnter" @mouseleave="mouseLeave" class="note" :style="{ 'background-color': note.theme }">
+                <div class="Container">
+                    <v-icon v-if="note.important" class="starIcon">mdi-pin</v-icon>
+                    <v-spacer></v-spacer>
+                    <div class="iconContainer">
+                        <v-icon class="deleteIcon" @click.prevent="deleteNote">mdi-close-circle</v-icon>
+                    </div>
+                </div>
+                <div v-bind="attrs" v-on="on" @click.prevent="initData">
+                    <v-card-title class="cardTitle noteTitle">
+                        <p>{{ note.title }}</p>
+                    </v-card-title>
+                    <v-card-text class="noteText">
+                        <p>{{ note.text }}</p>
+                    </v-card-text>
+                </div>
+            </v-card>
+        </template>
+        <ModifyEditor :note="note" @noteModified="modifyNote" />
     </v-dialog>
-  </div>
+</div>
 </template>
 
 <script>
 import ModifyEditor from "./ModifyEditor";
 export default {
-  props: {
-    note: {
-      type: Object,
-      required: true,
-    },
-    date: {
-      type: String,
-      required: true,
-    },
-  },
-
-  data () {
-    return {
-      dialog: false,
-      isSubmit: false,
-      tempNote: {},
-    };
-  },
-
-  watch: {
-    dialog: {
-      handler () {
-        if (this.dialog === false && this.isSubmit === false) {
-          console.log("대화상자 꺼짐!")
-          this.note.title = this.tempNote.title;
-          this.note.text = this.tempNote.text;
-          this.note.theme = this.tempNote.theme;
-          this.note.important = this.tempNote.important;
-        }
-      },
-    },
-  },
-
-  methods: {
-    mouseEnter (e) {
-      e.target.firstChild.lastChild.style.visibility = "visible";
+    props: {
+        note: {
+            type: Object,
+            required: true,
+        },
+        date: {
+            type: String,
+            required: true,
+        },
     },
 
-    mouseLeave (e) {
-      e.target.firstChild.lastChild.style.visibility = "hidden";
+    data() {
+        return {
+            dialog: false,
+            isSubmit: false,
+            tempNote: {},
+        };
     },
 
-    modifyNote (title, text, theme, time, date, originDate, important) {
-      this.isSubmit = true;
-      this.dialog = false;
-      this.$emit("modifyNote", title, text, theme, time, date, originDate, this.note.guid, important);
-
+    watch: {
+        dialog: {
+            handler() {
+                if (this.dialog === false && this.isSubmit === false) {
+                    this.note.title = this.tempNote.title;
+                    this.note.text = this.tempNote.text;
+                    this.note.theme = this.tempNote.theme;
+                    this.note.important = this.tempNote.important;
+                }
+            },
+        },
     },
 
-    initData () {
-      this.tempNote.title = this.note.title;
-      this.tempNote.text = this.note.text;
-      this.tempNote.theme = this.note.theme;
-      this.tempNote.important = this.note.important
+    methods: {
+        mouseEnter(e) {
+            e.target.firstChild.lastChild.style.visibility = "visible";
+        },
 
-      this.isSubmit = false;
+        mouseLeave(e) {
+            e.target.firstChild.lastChild.style.visibility = "hidden";
+        },
+
+        modifyNote(title, text, theme, time, date, originDate, important) {
+            this.isSubmit = true;
+            this.dialog = false;
+            this.$emit("modifyNote", title, text, theme, time, date, originDate, this.note.guid, important);
+
+        },
+
+        initData() {
+            this.tempNote.title = this.note.title;
+            this.tempNote.text = this.note.text;
+            this.tempNote.theme = this.note.theme;
+            this.tempNote.important = this.note.important
+
+            this.isSubmit = false;
+        },
+
+        deleteNote() {
+            if (confirm("정말 삭제하시겠습니까?"))
+                this.$emit("deleteNote", this.note.guid);
+        },
     },
 
-    deleteNote () {
-      this.$emit("deleteNote", this.note.guid);
+    components: {
+        ModifyEditor,
     },
-  },
-
-  components: {
-    ModifyEditor,
-  },
 };
 </script>
 
 <style scoped>
 .cardTitle {
-  margin-top: -25px;
+    margin-top: -25px;
 }
 
 .Container {
-  display: flex;
+    display: flex;
 }
 
 .iconContainer {
-  visibility: hidden;
+    visibility: hidden;
 }
 
 .deleteIcon:hover {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 p {
-  white-space: pre-line;
+    white-space: pre-line;
 }
 </style>
