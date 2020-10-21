a76bfd05a81819bb953afdc519105a4edea808a8
20160450 <olympiodoros@naver.com>
2020-10-22 01:20:23 +0900

    Localstorage Connection Complete

--- vue-note-app/src/components/CategoryBtn.vue
+++ vue-note-app/src/components/CategoryBtn.vue
@@ -2,12 +2,31 @@
 <div>
     <v-dialog v-model="dialog" width="500">
         <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on" class="mx-2 categoryBtn" fab dark color="blue">
+            <v-btn v-bind="attrs" v-on="on" class="mx-2 categoryBtn" fab dark color="blue" @click="initData">
                 <v-icon dark> mdi-tag-multiple </v-icon>
             </v-btn>
         </template>
 
         <v-card>
+            <v-card-title class="cardTitle">Category</v-card-title>
+            <v-card-text class="mt-5">
+                <p>Registered Categories</p>
+
+                <v-chip class="category" v-for="(tag, index) in tags" :key="`category-${index}`" close @click:close="deleteTag(index)">
+                    {{ tag }}
+                </v-chip>
+
+                <v-divider></v-divider>
+
+                <v-row class="inputContainer">
+                    <v-col cols="12" lg="8" md="8" sm="12">
+                        <v-text-field color="blue" placeholder="Type your category..." hide-details prepend-icon="mdi-tag" single-line type="text" v-model="text"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" lg="4" md="4" sm="12" style="text-align:center;">
+                        <v-btn text @click="registerCategory">register</v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-text>
 
         </v-card>
     </v-dialog>
@@ -18,7 +37,41 @@
 export default {
     data() {
         return {
-            dialog: false
+            dialog: false,
+            tags: [],
+            text: ""
+        }
+    },
+    mounted() {
+        if (localStorage.getItem("category"))
+            this.tags = JSON.parse(localStorage.getItem('category'));
+    },
+
+    watch: {
+        tags: {
+            handler() {
+                var newTags = this.tags;
+                localStorage.setItem("category", JSON.stringify(newTags));
+            }
+        }
+    },
+
+    methods: {
+        initData() {
+            this.text = ""
+        },
+
+        deleteTag(index) {
+            this.tags.splice(index, 1);
+        },
+
+        registerCategory() {
+            if (this.text === "") {
+                alert("카테고리를 입력해주세요");
+                return;
+            }
+            this.tags.push(this.text);
+            this.text = "";
         }
     }
 
@@ -31,5 +84,19 @@ export default {
     right: 5%;
     bottom: 35%;
     z-index: 10;
+}
+
+.cardTitle {
+    background: #2196f3;
+    color: white;
+}
+
+.inputContainer {
+    display: flex;
+    align-items: center;
+}
+
+.category {
+    margin: 2px;
 }
 </style>
--- vue-note-app/src/components/ModifyEditor.vue
+++ vue-note-app/src/components/ModifyEditor.vue
@@ -1,130 +1,117 @@
 <template>
-  <div>
+<div>
     <v-card class="dialogBox">
-      <v-card-title
-        class="headline lighten-2"
-        :style="{ backgroundColor: note.theme }"
-      >
-        <textarea v-model="note.title" cols="65" placeholder="Title"></textarea>
-        <v-spacer></v-spacer>
-        <div class="time">{{ note.time }}</div>
-      </v-card-title>
+        <v-card-title class="headline lighten-2" :style="{ backgroundColor: note.theme }">
+            <textarea v-model="note.title" cols="65" placeholder="Title"></textarea>
+            <v-spacer></v-spacer>
+            <div class="time">{{ note.time }}</div>
+        </v-card-title>
 
-      <v-card-text>
-        <textarea
-          v-model="note.text"
-          placeholder="Take a note..."
-          cols="65"
-          rows="17"
-        ></textarea>
-      </v-card-text>
+        <v-card-text>
+            <textarea v-model="note.text" placeholder="Take a note..." cols="65" rows="17"></textarea>
+        </v-card-text>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-card-actions>
-        <Color :theme="note.theme" @selectedColor="initColor" />
-        <v-icon
-          class="starIcon"
-          :class="{ important: note.important }"
-          @click="addImportant"
-          >mdi-star</v-icon
-        >
-        <v-spacer></v-spacer>
-        <v-btn color="black" text @click="modifyNote"> modify </v-btn>
-      </v-card-actions>
+        <v-card-actions>
+            <Color :theme="note.theme" @selectedColor="initColor" />
+            <v-icon class="starIcon" :class="{ important: note.important }" @click="addImportant">mdi-star</v-icon>
+            <v-spacer></v-spacer>
+            <v-btn color="black" text @click="modifyNote"> modify </v-btn>
+        </v-card-actions>
     </v-card>
-  </div>
+</div>
 </template>
 
 <script>
 import Color from "./Color";
 export default {
-  props: {
-    note: {
-      type: Object,
-      required: true,
-    },
-  },
-
-  methods: {
-    initColor (picker) {
-      this.note.theme = picker;
+    props: {
+        note: {
+            type: Object,
+            required: true,
+        },
     },
 
-    modifyNote () {
-      if (this.note.title === "" || this.note.text === "") {
-        alert("제목이나 내용을 입력해주세요");
-        return;
-      }
+    methods: {
+        initColor(picker) {
+            this.note.theme = picker;
+        },
 
-      const originDate = this.note.date;
+        modifyNote() {
+            if (this.note.title === "" || this.note.text === "") {
+                alert("제목이나 내용을 입력해주세요");
+                return;
+            }
 
-      const dateObj = new Date();
+            const originDate = this.note.date;
 
-      const year = dateObj.getFullYear();
-      const month = dateObj.getMonth() + 1;
-      const day = dateObj.getDate();
+            const dateObj = new Date();
 
-      let hour = dateObj.getHours();
-      let minutes = dateObj.getMinutes();
-      let seconds = dateObj.getSeconds();
+            const year = dateObj.getFullYear();
+            const month = dateObj.getMonth() + 1;
+            const day = dateObj.getDate();
 
-      if (minutes < 10) minutes = "0" + minutes;
-      if (seconds < 10) seconds = "0" + seconds;
+            let hour = dateObj.getHours();
+            let minutes = dateObj.getMinutes();
+            let seconds = dateObj.getSeconds();
 
-      const date = `${year}-${month}-${day}`;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (seconds < 10) seconds = "0" + seconds;
 
-      const time = `${hour}:${minutes}:${seconds}`;
+            const date = `${year}-${month}-${day}`;
 
-      this.$emit(
-        "noteModified",
-        this.note.title,
-        this.note.text,
-        this.note.theme,
-        time,
-        date,
-        originDate,
-        this.note.important
-      );
+            const time = `${hour}:${minutes}:${seconds}`;
+
+            this.$emit(
+                "noteModified",
+                this.note.title,
+                this.note.text,
+                this.note.theme,
+                time,
+                date,
+                originDate,
+                this.note.important
+            );
+        },
+        addImportant() {
+            this.note.important = !this.note.important
+        },
+
     },
-    addImportant () {
-      this.note.important = !this.note.important
+
+    components: {
+        Color,
     },
-
-  },
-
-  components: {
-    Color,
-  },
 };
 </script>
 
 <style scoped>
 textarea {
-  padding: 10px;
-  resize: none;
-  border: none;
+    padding: 10px;
+    resize: none;
+    border: none;
 }
 
 textarea:hover {
-  outline: 0;
+    outline: 0;
 }
 
 textarea:focus {
-  outline: 0;
+    outline: 0;
 }
 
 .dialogBox {
-  overflow: hidden;
+    overflow: hidden;
 }
 
 .time {
-  color: rgb(102, 102, 102);
-  font-size: 15px;
-  margin-bottom: -10px;
+    color: rgb(102, 102, 102);
+    font-size: 15px;
+    margin-bottom: -10px;
 }
 
 .important {
-  color: yellow;
+    color: yellow;
 }
 </style>
