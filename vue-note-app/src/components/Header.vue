df9555e5a60b9c2a6f90f3c00baf50729ea55aa4
20160450 <olympiodoros@naver.com>
2020-10-23 16:10:20 +0900

    Separate delete buttons on mobile screen

--- vue-note-app/src/components/Header.vue
+++ vue-note-app/src/components/Header.vue
@@ -1,54 +1,54 @@
 <template>
-  <div class="header">
+<div class="header">
     <img src="../assets/50VemoBlue.png" />
     <v-spacer></v-spacer>
     <p>Now Date is {{ this.date }}</p>
     <v-spacer></v-spacer>
     <SearchBar @submitText="submitText" />
-  </div>
+</div>
 </template>
 
 <script>
 import SearchBar from './SearchBar'
 export default {
-  props: {
-    date: {
-      type: String,
-      required: true,
+    props: {
+        date: {
+            type: String,
+            required: true,
+        },
     },
-  },
 
-  methods: {
-    submitText (text) {
-      this.$emit("searchNote", text);
+    methods: {
+        submitText(text) {
+            this.$emit("searchNote", text);
+        }
+    },
+
+    components: {
+        SearchBar
     }
-  },
-
-  components: {
-    SearchBar
-  }
 };
 </script>
 
 <style scoped>
 .header {
-  display: flex;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.741);
-  color: #2196f3;
-  position: fixed;
-  left: 0;
-  right: 0;
+    display: flex;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.741);
+    color: #2196f3;
+    position: fixed;
+    left: 0;
+    right: 0;
 }
 
 img {
-  margin-left: 20px;
+    margin-left: 20px;
 }
 
 p {
-  font-size: 25px;
-  margin: 25px;
-  margin-left: 11%;
-  font-family: "Sansita Swashed", cursive;
+    font-size: 25px;
+    margin: 25px;
+    margin-left: 11%;
+    font-family: "Sansita Swashed", cursive;
 }
 </style>
