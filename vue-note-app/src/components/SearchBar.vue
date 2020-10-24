886ffd3f4791b1305718226dc7032b75d3cd8fd4
20160450 <olympiodoros@naver.com>
2020-10-24 18:35:31 +0900

    Fixed a bug where other notes would join when they were deleted or modified

--- vue-note-app/src/components/SearchBar.vue
+++ vue-note-app/src/components/SearchBar.vue
@@ -1,37 +1,28 @@
 <template>
-  <div>
-    <v-text-field
-      class="searchBar"
-      placeholder="Type note..."
-      hide-details
-      prepend-icon="mdi-magnify"
-      single-line
-      v-model="text"
-      type="text"
-      @input="submitText"
-    ></v-text-field>
-  </div>
+<div>
+    <v-text-field class="searchBar" placeholder="Type note..." hide-details prepend-icon="mdi-magnify" single-line v-model="text" type="text" @input="submitText"></v-text-field>
+</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      text: ""
-    }
-  },
-
-  methods: {
-    submitText () {
-      this.$emit("submitText", this.text);
+    data() {
+        return {
+            text: ""
+        }
     },
 
-  }
+    methods: {
+        submitText() {
+            this.$emit("submitText", this.text);
+        },
+
+    }
 }
 </script>
 
 <style scoped>
 .searchBar {
-  margin-right: 15px;
+    margin-right: 15px;
 }
 </style>
