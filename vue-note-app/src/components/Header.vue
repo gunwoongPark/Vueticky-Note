7018f91e4a964b8b032fd29232e5709dea92169f
20161313 <dbstjd427@gmail.com>
2020-10-25 15:23:55 +0900

    Reflect master Header layout

--- vue-note-app/src/components/Header.vue
+++ vue-note-app/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
-    <v-container fluid>
-      <div class="PCView hidden-sm-and-down">
+    <v-container>
+      <!-- <div class="PCView hidden-sm-and-down">
         <v-row>
           <v-col style="text-align: right" cols="4"
             ><img class="logo" src="../assets/Vemomint.png"
@@ -59,7 +59,34 @@
             </v-sheet>
           </v-col>
         </v-row>
-      </div>
+      </div> -->
+
+      <v-row class="searchContainer">
+        <v-col cols="0" lg="3" md="2" sm="1"></v-col>
+        <v-col cols="3" lg="1" md="2" sm="2">
+          <img class="logo" src="../assets/Vemomint.png" />
+        </v-col>
+        <v-col cols="9" lg="5" md="6" sm="8">
+          <SearchBar @submitText="submitText"
+        /></v-col>
+        <v-col cols="0" lg="3" md="2" sm="1"></v-col>
+      </v-row>
+      <v-row>
+        <v-col class="tagContainer"
+          ><v-slide-group class="tagBar" show-arrows>
+            <v-slide-item
+              style="background-color: #00bfa5"
+              class="tagItems"
+              v-for="(tag, index) in tags"
+              :key="`tag-${index}`"
+            >
+              <v-btn class="mx-2" depressed rounded @click="selectTag(index)">
+                {{ tag }}
+              </v-btn>
+            </v-slide-item>
+          </v-slide-group></v-col
+        >
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -91,10 +118,6 @@ export default {
 </script>
 
 <style scoped>
-.gr {
-  border: solid 1px lightgray;
-}
-
 .header {
   display: flex;
   background: #e0f2f1c2;
@@ -105,7 +128,18 @@ export default {
 }
 
 .logo {
-  margin-top: -5px;
+  margin: -30px 0 0 10px;
+}
+
+.searchContainer {
+  display: flex;
+  align-items: center;
+}
+
+.tagContainer {
+  margin-top: -40px;
+  display: flex;
+  justify-content: center;
 }
 </style>
 
